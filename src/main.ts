8de15f196d22c94d27f9b86c9485c23bd9fadb3e
apertsovsky <alexander.pertsovsky@gmail.com>
2020-11-08 14:35:23 +0300

    Updated exception handling in main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,22 +20,32 @@ const logger: Logger = createLogger();
     });
  
     let lawn: Lawn = null as any;
-    let mower: Mower = null as any;
+    let mowerConfigLine: string = '';
+    let fileReporter: FileReporter = new FileReporter("output.txt");
     try {
         for await (const line of readLine) {
             if (lawn === null) {
                 lawn = createLawn(line); 
                 logger.debug('Lawn initialized');  
             } else {
-                if (mower === null) {
-                    mower = createMower(lawn, line, new FileReporter("output.txt"));
-                    logger.debug('Mower initialized'); 
+                if (mowerConfigLine === '') {
+                    mowerConfigLine = line;
                 } else {
-                    executeCommands(mower, line);
-                    logger.debug('Mower completed'); 
-                    mower = null as any;
+                    try {
+                        let mower: Mower = createMower(lawn, mowerConfigLine, fileReporter);
+                        await executeCommands(mower, line);
+                        logger.debug('Mower completed');
+                    } catch (error) {
+                        logger.error(`Failed to proces mower ${error}`);
+                        await fileReporter.report('Failed to process mower see logs.txt for details');    
+                    }
+                    mowerConfigLine = '';
                 }
             }
+        }
+        if (mowerConfigLine !== '') {
+            logger.warn('Last mower has no commands to execute');
+            await fileReporter.report('Last mower has no commands to execute'); 
         }
     } catch (error) {
         logger.error(`Error occured ${error}`)
